03fdcf082d8c1af7f9827297b74c6315ea691899
Devdeep Patidar <patidardevdeep999@gmail.com>
2024-04-29 03:11:17 +0530

    linting

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
+import NextAuth,  from "next-auth";
 // import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
