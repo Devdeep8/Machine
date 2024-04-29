ecb033b222d70b8c03b684b7b48babfd246cdf3a
Devdeep Patidar <patidardevdeep999@gmail.com>
2024-04-29 10:51:40 +0530

    auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,7 +10,7 @@ export const authOptions  = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXT_PUBLIC_SECRET
+  secret: process.env.NEXTAUTH_SECRET
 }; 
 
 const handler = NextAuth(authOptions);
